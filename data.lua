14b8e9c9febd492c0a592fabf31e9dbb2f96a269
Phansteck <phansteck@gmail.com>
2025-12-07 14:14:54 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -3,23 +3,56 @@
 -- Data stage loader
 --------------------------------------------------------------------------------
 
+------------------------------
 -- Core utilities
+------------------------------
 require("prototypes.urob-core.functions")
 
--- Groups & subgroups
+------------------------------
+-- Groups & shared constants
+------------------------------
 require("prototypes.groups.item-groups")
+require("prototypes.commons")  -- shared colors, globals, etc.
 
--- Shared color definitions
-require("prototypes.commons")
-
--- Items
-require("prototypes.items.crushed-ores")
-
--- Buildings
-require("prototypes.buildings.ore-crusher")
-
+------------------------------
 -- Categories
+------------------------------
 require("prototypes.categories.crushing")
 
--- Recipes (later)
+------------------------------
+-- Items
+------------------------------
+require("prototypes.items.crushed-ores")
+-- Add later:
+-- require("prototypes.items.ores")
+-- require("prototypes.items.intermediates")
+-- require("prototypes.items.pure-ores")
+
+------------------------------
+-- Buildings
+------------------------------
+require("prototypes.buildings.ore-crusher")
+-- Add later:
+-- require("prototypes.buildings.ore-washer")
+-- require("prototypes.buildings.sorter")
+
+------------------------------
+-- Recipes
+------------------------------
+-- Crushing tier recipes
 -- require("prototypes.recipes.crushing")
+
+-- Refining chain (Angel-style)
+-- require("prototypes.recipes.ore-processing.crushing")
+-- require("prototypes.recipes.ore-processing.washing")
+-- require("prototypes.recipes.ore-processing.sorting")
+
+------------------------------
+-- Technology (future)
+------------------------------
+-- require("prototypes.technology.crushing")
+-- require("prototypes.technology.refining")
+
+--------------------------------------------------------------------------------
+-- END OF DATA STAGE
+--------------------------------------------------------------------------------
