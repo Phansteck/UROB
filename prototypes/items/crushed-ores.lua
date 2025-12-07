74b113e8c9c11813a7f291f4b1b744615324772e
Phansteck <phansteck@gmail.com>
2025-12-07 10:53:33 +0100

    Create crushed-ores.lua

---
+++ prototypes/items/crushed-ores.lua
@@ -0,0 +1,21 @@
+--------------------------------------------------------------------------------
+-- UR0B: Crushed ore items (iron & copper)
+-- These are the basic intermediate resources produced by early crushing.
+--------------------------------------------------------------------------------
+
+data:extend({
+  {
+    type = "item",
+    name = "urob-crushed-iron",
+    icon = "__UR0B__/graphics/icons/crushed-iron.png", -- placeholder path
+    icon_size = 64,
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "urob-crushed-copper",
+    icon = "__UR0B__/graphics/icons/crushed-copper.png", -- placeholder path
+    icon_size = 64,
+    stack_size = 200
+  }
+})
