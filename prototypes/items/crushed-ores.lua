a7830e1768199cc134fee6843599b3e4b4e93301
Phansteck <phansteck@gmail.com>
2025-12-07 12:02:15 +0100

    Update crushed-ores.lua

--- prototypes/items/crushed-ores.lua
+++ prototypes/items/crushed-ores.lua
@@ -1,21 +1,49 @@
 --------------------------------------------------------------------------------
--- UR0B: Crushed ore items (iron & copper)
--- These are the basic intermediate resources produced by early crushing.
+-- File: prototypes/items/crushed-ores.lua
+-- Purpose: Crushed ore item definitions for UR0B.
+-- These use a single base icon with ore-specific tints defined in commons.lua.
 --------------------------------------------------------------------------------
 
 data:extend({
+
+  -- ========================================================================
+  -- Crushed Iron
+  -- ========================================================================
   {
     type = "item",
     name = "urob-crushed-iron",
-    icon = "__UR0B__/graphics/icons/crushed-iron.png", -- placeholder path
-    icon_size = 64,
+    icons = {{
+      icon = "__UR0B__/graphics/icons/crushed-ore.png",
+      tint = urob_colors.iron   -- Realistic Ores tint
+    }},
+    icon_size = 32,
     stack_size = 200
   },
+
+  -- ========================================================================
+  -- Crushed Copper
+  -- ========================================================================
   {
     type = "item",
     name = "urob-crushed-copper",
-    icon = "__UR0B__/graphics/icons/crushed-copper.png", -- placeholder path
-    icon_size = 64,
+    icons = {{
+      icon = "__UR0B__/graphics/icons/crushed-ore.png",
+      tint = urob_colors.copper  -- Realistic Ores tint
+    }},
+    icon_size = 32,
     stack_size = 200
   }
+
+  -- Add more crushed ores here when needed:
+  -- {
+  --   type = "item",
+  --   name = "urob-crushed-tin",
+  --   icons = {{
+  --     icon = "__UR0B__/graphics/icons/crushed-ore.png",
+  --     tint = urob_colors.tin
+  --   }},
+  --   icon_size = 32,
+  --   stack_size = 200
+  -- },
+
 })
