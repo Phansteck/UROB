b50c7769db031819e07b7e648b0ee7643c7d373d
Phansteck <phansteck@gmail.com>
2025-12-07 10:39:06 +0100

    Create data-updates.lua

---
+++ data-updates.lua
@@ -0,0 +1,8 @@
+-- UR0B: Unified Refining Oversight Bureau
+-- Data-updates stage: adjustments to vanilla prototypes and map generation.
+
+-- Disable vanilla smelting of iron and copper ore.
+require("prototypes.overrides.disable-vanilla-smelting")
+
+-- Remove stone patches from map generation and hide them from the map gen GUI.
+require("prototypes.overrides.remove-stone-autoplace")
