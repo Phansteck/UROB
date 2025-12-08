c518c63b06e32156b779dba4dfa8e82887898bcd
Phansteck <phansteck@gmail.com>
2025-12-08 13:12:50 +0100

    Update control.lua

--- control.lua
+++ control.lua
@@ -5,29 +5,23 @@ local function give_starting_items(event)
     local player = game.get_player(event.player_index)
     if not player or not player.valid then return end
 
-    -- Prevents giving items on multiplayer late-joins
+    -- Prevents giving items on multiplayer late joins
     if not game.is_multiplayer() and player.index ~= 1 then return end
 
-    -- Settings
-    local give_crusher = settings.global["urob-start-with-burner-crusher"].value
-    local give_coal = settings.global["urob-start-with-coal"].value
+    -- Startup setting
+    local give_crusher = settings.startup["urob-start-with-burner-crusher"].value
 
     -- Burner Crusher
     if give_crusher then
         if player.insert({ name = "burner-ore-crusher", count = 1 }) > 0 then
-            -- Place in hotbar slot 1 if empty
+            -- Slot 1 on quickbar if empty
             if player.get_quick_bar()[1] == nil then
                 player.set_quick_bar_slot(1, "burner-ore-crusher")
             end
         end
     end
 
-    -- Starting Coal
-    if give_coal then
-        player.insert({ name = "coal", count = 10 })
-    end
-
-    -- Localized message
+    -- Message
     player.print({ "message.urob-start-equipment" })
 end
 
