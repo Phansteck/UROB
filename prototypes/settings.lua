178b658e895b7245b6d3e28e30305efca25890ae
Phansteck <phansteck@gmail.com>
2025-12-08 13:12:39 +0100

    Update settings.lua

--- prototypes/settings.lua
+++ prototypes/settings.lua
@@ -2,15 +2,8 @@ data:extend({
   {
     type = "bool-setting",
     name = "urob-start-with-burner-crusher",
-    setting_type = "runtime-global",
+    setting_type = "startup",
     default_value = true,
     order = "a",
   },
-  {
-    type = "bool-setting",
-    name = "urob-start-with-coal",
-    setting_type = "runtime-global",
-    default_value = true,
-    order = "b",
-  }
 })
