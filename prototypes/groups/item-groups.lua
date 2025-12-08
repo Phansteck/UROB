25a00069a9c337563345c47a7449adb751decd1f
Phansteck <phansteck@gmail.com>
2025-12-08 11:16:49 +0100

    Update item-groups.lua

--- prototypes/groups/item-groups.lua
+++ prototypes/groups/item-groups.lua
@@ -4,15 +4,7 @@
 -- This file defines the tabs at the top of the crafting GUI (item-groups)
 -- and the rows inside those tabs (item-subgroups) for the UR0B mod.
 --
--- Philosophy:
--- - One main group for all ore/refining chains.
--- - A separate group for UR0B buildings (crushers, washers, sorters, etc.).
--- - Subgroups follow an Angel-ish flow: crushed → chunks → crystals → pure,
---   plus utility chains like slag/geodes and buildings.
---
--- NOTE:
--- - It’s safe if some subgroups are unused at first; Factorio doesn’t mind.
--- - You can add/remove subgroups later as the mod evolves.
+-- Updated: Refining group now uses Angels-style layered icon.
 
 data:extend({
   ------------------------------------------------------------------------------
@@ -21,17 +13,32 @@ data:extend({
 
   {
     type = "item-group",
-    name = "urob-refining",              -- All ore + intermediate materials
-    order = "u-a[urob-refining]",        -- Sorted early among modded groups
+    name = "urob-refining",
+    order = "u-a[urob-refining]",
     order_in_recipe = "u-a[urob-refining]",
-    -- Re-use a base icon so we don't rely on custom graphics yet.
-    icon = "__base__/graphics/icons/iron-ore.png",
-    icon_size = 64,
+
+    -- Angels-style ICONS block (multi-layer)
+    icons = {
+      {
+        -- Main tech-style icon (scaled down, just like Angels)
+        icon = "__UR0B__/graphics/technology/mechanical-refining.png",
+        icon_size = 256,
+        icon_mipmaps = 4,
+        scale = 0.25,
+      },
+      {
+        -- Corner overlay icon (Angels uses void.png)
+        icon = "__UR0B__/graphics/icons/void.png",
+        icon_size = 32,
+        scale = (64/32) * 0.35,
+        shift = { 20, -20 },
+      },
+    },
   },
 
   {
     type = "item-group",
-    name = "urob-buildings",             -- All UR0B machines & structures
+    name = "urob-buildings",
     order = "u-b[urob-buildings]",
     order_in_recipe = "u-b[urob-buildings]",
     icon = "__base__/graphics/icons/assembling-machine-2.png",
@@ -42,7 +49,6 @@ data:extend({
   -- REFINING CHAIN SUBGROUPS (materials, inside urob-refining)
   ------------------------------------------------------------------------------
 
-  -- Raw mined stuff from UR0B (if you have custom ores later, put them here)
   {
     type = "item-subgroup",
     name = "urob-ore-raw",
@@ -50,7 +56,6 @@ data:extend({
     order = "a[ore-raw]",
   },
 
-  -- Crushed ores (output of the first crusher tier)
   {
     type = "item-subgroup",
     name = "urob-ore-crushed",
@@ -58,7 +63,6 @@ data:extend({
     order = "b[ore-crushed]",
   },
 
-  -- Chunks (second step in the Angel-style chain)
   {
     type = "item-subgroup",
     name = "urob-ore-chunk",
@@ -66,7 +70,6 @@ data:extend({
     order = "c[ore-chunk]",
   },
 
-  -- Crystals (third step in the chain)
   {
     type = "item-subgroup",
     name = "urob-ore-crystal",
@@ -74,7 +77,6 @@ data:extend({
     order = "d[ore-crystal]",
   },
 
-  -- Pure ores / final refined intermediates before smelting or direct use
   {
     type = "item-subgroup",
     name = "urob-ore-pure",
@@ -82,7 +84,6 @@ data:extend({
     order = "e[ore-pure]",
   },
 
-  -- Slag, by-products, and their processing chain
   {
     type = "item-subgroup",
     name = "urob-slag-processing",
@@ -90,7 +91,6 @@ data:extend({
     order = "f[slag-processing]",
   },
 
-  -- Geodes / crystals / weird side products (Angel-style)
   {
     type = "item-subgroup",
     name = "urob-geode-processing",
@@ -98,7 +98,6 @@ data:extend({
     order = "g[geode-processing]",
   },
 
-  -- Fluids and water treatment related to UR0B (mineralized water etc.)
   {
     type = "item-subgroup",
     name = "urob-water-treatment",
@@ -106,7 +105,6 @@ data:extend({
     order = "h[water-treatment]",
   },
 
-  -- Generic “other intermediates” (catalysts, filter media, etc.)
   {
     type = "item-subgroup",
     name = "urob-intermediates",
@@ -118,7 +116,6 @@ data:extend({
   -- BUILDINGS SUBGROUPS (machines, inside urob-buildings)
   ------------------------------------------------------------------------------
 
-  -- Crushers (mk1–mk3 etc., following the Angels naming scheme for entities)
   {
     type = "item-subgroup",
     name = "urob-buildings-crushers",
@@ -126,7 +123,6 @@ data:extend({
     order = "a[buildings-crushers]",
   },
 
-  -- Ore sorters / washing / flotation / leaching machines and similar
   {
     type = "item-subgroup",
     name = "urob-buildings-refining",
@@ -134,7 +130,6 @@ data:extend({
     order = "b[buildings-refining]",
   },
 
-  -- Water treatment / filtration, clarifiers, etc.
   {
     type = "item-subgroup",
     name = "urob-buildings-fluids",
@@ -142,7 +137,6 @@ data:extend({
     order = "c[buildings-fluids]",
   },
 
-  -- Any support structures (warehouses, depots, special chests, etc.)
   {
     type = "item-subgroup",
     name = "urob-buildings-support",
@@ -150,7 +144,6 @@ data:extend({
     order = "d[buildings-support]",
   },
 
-  -- Catch-all for anything UR0B-building-ish you haven’t sorted yet
   {
     type = "item-subgroup",
     name = "urob-buildings-misc",
