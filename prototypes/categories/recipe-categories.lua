b97b0fe8f36b68ca4e620aec6ceb2d1716db5117
Phansteck <phansteck@gmail.com>
2025-12-08 11:24:51 +0100

    Update recipe-categories.lua

--- prototypes/categories/recipe-categories.lua
+++ prototypes/categories/recipe-categories.lua
@@ -1,6 +1,15 @@
+-- File: prototypes/categories/recipe-categories.lua
+-- Defines custom recipe categories used by UR0B
+
 data:extend({
+
+  ------------------------------------------------------------------------------
+  -- CRUSHING
+  -- Used by: burner crusher, ore-crusher MK1, MK2, MK3
+  ------------------------------------------------------------------------------
   {
     type = "recipe-category",
     name = "urob-crushing"
-  }
+  },
+
 })
